dc8627cf1cda78e695d5d1f4a729a2c482e669e9
Juraj Kostolansky <juraj@kostolansky.sk>
2025-09-23 19:19:35 +0200

    feat: Allow image pinch to zoom on mobile

--- app/javascript/controllers/lightbox_controller.js
+++ app/javascript/controllers/lightbox_controller.js
@@ -8,17 +8,32 @@ export default class extends Controller {
 
     this.dialogTarget.showModal()
     this.#set(event.target.closest("a"))
+    this.#toggleViewportZoom(true)
   }
 
   reset() {
     this.zoomedImageTarget.src = ""
     this.downloadTarget.href = ""
     this.shareTarget.dataset.webShareFilesValue = "";
+    this.#toggleViewportZoom(false)
   }
 
   #set(target) {
     this.zoomedImageTarget.src = target.href
     this.downloadTarget.href = target.dataset.lightboxUrlValue;
     this.shareTarget.dataset.webShareFilesValue = target.dataset.lightboxUrlValue;
+  }
+
+  #toggleViewportZoom(allowZoom) {
+    const viewportMeta = document.querySelector('meta[name="viewport"]')
+
+    if (viewportMeta) {
+      if (allowZoom) {
+        this.originalViewportContent = viewportMeta.content
+        viewportMeta.content = this.originalViewportContent.replace(/user-scalable=(no|0)/, "user-scalable=yes")
+      } else if (this.originalViewportContent) {
+        viewportMeta.content = this.originalViewportContent
+      }
+    }
   }
 }
