7845ae2ca588450b6727649eff0cb47a9718ac28
AdityaTeltia <teltia.aditya22@gmail.com>
2025-09-24 23:43:47 +0530

    refactor: improve robustness,scalability of room broadcasts and handles edge cases

--- app/jobs/room_update_broadcast_job.rb
+++ app/jobs/room_update_broadcast_job.rb
@@ -1,41 +1,16 @@
 class RoomUpdateBroadcastJob < ApplicationJob
   queue_as :default
 
+  rescue_from ActiveJob::DeserializationError do
+  end
+
   def perform(room)
-    for_each_sidebar_section do |list_name|
-      each_user_and_html_for(room, list_name:) do |user, html|
-        broadcast_replace_to user, :rooms, target: [ room, helpers.dom_prefix(list_name, :list_node) ], html: html
-      end
+    # Throttle updates to once every 5 seconds per room to avoid a thundering herd problem.
+    lock_key = "room_update_broadcast_job_lock:#{room.id}"
+    return unless Kredis.redis.set(lock_key, "1", nx: true, ex: 5)
+
+    room.memberships.visible.find_each do |membership|
+      RoomMembershipBroadcastJob.perform_later(membership)
     end
-  end
-
-  private
-
-  def each_user_and_html_for(room, **locals)
-    html_cache = {}
-
-    room.memberships.visible.includes(:user).with_has_unread_notifications.each do |membership|
-      yield membership.user, render_or_cached(html_cache,
-                                              partial: "users/sidebars/rooms/shared",
-                                              locals: { membership: membership, list_name: locals[:list_name], room: room })
-    end
-  end
-
-  def render_or_cached(cache, partial:, locals:)
-    cache[locals[:list_name]] ||= ApplicationController.render(partial: partial, locals: locals)
-  end
-
-  def for_each_sidebar_section
-    [ :starred_rooms, :shared_rooms ].each do |name|
-      yield name
-    end
-  end
-
-  def broadcast_replace_to(stream, *args)
-    Turbo::StreamsChannel.broadcast_replace_to(stream, *args)
-  end
-
-  def helpers
-    ApplicationController.helpers
   end
 end
