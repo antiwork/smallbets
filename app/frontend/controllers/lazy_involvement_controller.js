e77a6637b702bb5d715ec5d6ec0d4fa03e05c957
saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>
2025-12-27 01:24:17 +0530

    lint fix

--- app/frontend/controllers/lazy_involvement_controller.js
+++ app/frontend/controllers/lazy_involvement_controller.js
@@ -1,19 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static values = { src: String, loaded: Boolean }
+    static values = { src: String, loaded: Boolean }
 
-  connect() {
-    this.loadedValue = false
-  }
-
-  load() {
-    if (this.loadedValue) return
-
-    const frame = this.element
-    if (this.srcValue && !frame.src) {
-      frame.src = this.srcValue
-      this.loadedValue = true
+    connect() {
+        this.loadedValue = false
     }
-  }
+
+    load() {
+        if (this.loadedValue) return
+
+        const frame = this.element
+        if (this.srcValue && !frame.src) {
+            frame.src = this.srcValue
+            this.loadedValue = true
+        }
+    }
 }
