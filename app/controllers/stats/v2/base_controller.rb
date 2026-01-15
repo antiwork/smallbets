c290560638fc1d6d77c8faece8e06b185b41d64a
Renato Nitta <renatocn@gmail.com>
2026-01-15 00:12:23 -0300

    Removes unnecessary comment

--- app/controllers/stats/v2/base_controller.rb
+++ app/controllers/stats/v2/base_controller.rb
@@ -3,7 +3,6 @@
 module Stats
   module V2
     # Base controller for Stats V2
-    # Follows Single Responsibility Principle - provides common controller behavior
     class BaseController < ApplicationController
       layout 'application'
     end
